45664095f2e4397a6784450c0b7adee824216d33
tarikaytek <46650139+tarikaytek@users.noreply.github.com>
2022-06-15 17:19:30 +0300

    ready2go

--- np-sum-and-prod.py
+++ np-sum-and-prod.py
@@ -1,1 +1,10 @@
 # https://www.hackerrank.com/challenges/np-sum-and-prod/problem
+
+import numpy as np
+
+n, m = map(int, input().split())
+
+arr = np.array([input().split() for _ in range(n)], int)
+
+print(np.prod(np.sum(arr, axis= 0)))
+
